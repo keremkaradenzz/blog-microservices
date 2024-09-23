199093655e10789a17e1f05da6da3ade0da7ee7b
Kerem Karadeniz <kerem.karadeniz@commencis.com>
2024-09-23 21:42:39 +0300

    feat: created event bus settings

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,10 +10,10 @@ app.post("/events", (req, res) => {
   const event = req.body;
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  //axios.post("http://localhost:4002/events", event);    
   res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-  console.log("listening server port on 4005");
+  console.log(`Server is listening on ${4005} PORT`);
 });
